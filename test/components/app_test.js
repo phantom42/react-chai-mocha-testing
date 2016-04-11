db2de472191f483668ccf55556079dc11b2ac264
Joe Coleman <phantom42@gmail.com>
2016-04-11 00:37:43 -0400

    setup test spec

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,21 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+// use describe to group together similar tests
+//descriptive string. does not need to match anything
+describe('App', () => {
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
+	// use 'it' to test a single attribute of a target
+	// explanation of test
+	it('shows the correct text', () =>{
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
-});
+		// create an instance of app
+		const component = renderComponent(App);
+
+		// use expect to make an assertion about a target
+		expect(component).to.contain('React simple starter');
+	}); 
+
+	
+}); 
+
